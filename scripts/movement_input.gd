25b9d95d27e67d1965c20e08081880b91381c2c6
Brooklyn <brooklyn.dipietrantonio@gmail.com>
2026-04-18 15:02:29 -0500

    Fixed script. Deleted old v of player agent

--- scripts/movement_input.gd
+++ scripts/movement_input.gd
@@ -2,26 +2,16 @@ class_name MovementInput
 extends Node3D
 
 @onready var currentlySelectedPlayer : PlayerAgent = $Robot
+@onready var currentCamera : Camera3D = get_viewport().get_camera_3d()
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("left_mouse_click"):
-		var mouseIntersectionVector = _get_3d_mouse_position(event.position)
+		_handle_user_playfield_click(event.position)
 		
 		# If intersecting with PlayerAgent, select PlayerAgent and DON'T move. 
 		# Else, move currently selected PlayerAgent.
 		
-		currentlySelectedPlayer.set_target_position(mouseIntersectionVector)
-		
-		#if true:
-			#pass
-		#else:
-			#var player = currentlySelectedPlayer
-			#player.set_target_position(mouseIntersectionVector)
-				
-
-		
-func _get_3d_mouse_position(mousePosition2D):
-	var currentCamera = get_viewport().get_camera_3d()
+func _handle_user_playfield_click(mousePosition2D):
 	var params = PhysicsRayQueryParameters3D.new()
 	
 	params.from = currentCamera.project_ray_origin(mousePosition2D)
@@ -30,8 +20,10 @@ func _get_3d_mouse_position(mousePosition2D):
 	var worldspace = get_world_3d().direct_space_state
 	var result = worldspace.intersect_ray(params)
 	
-	if result:
-		return result.position
+	if result.collider is PlayerAgent:
+		currentlySelectedPlayer = result.collder
+	else:
+		currentlySelectedPlayer.set_target_position(result.position)
 
 func _set_currently_selected_player(selectedPlayer : PlayerAgent):
 	currentlySelectedPlayer = selectedPlayer
