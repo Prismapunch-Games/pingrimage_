3df043eb98811bf9f085b12e64407349af3f079f
Brooklyn <brooklyn.dipietrantonio@gmail.com>
2026-04-19 02:14:06 -0500

    Adjusted inputs

--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void: # This event is invoked on any input, in which we can decide what to do with the 'event' param.
 	if Input.is_action_just_pressed("primary_click"):
-		_handle_user_playfield_click(event.position)
+		_handle_user_agent_selection(event.position)
 		
 	if Input.is_action_just_pressed("secondary_click"):
 		_handle_user_playfield_click(event.position)
@@ -29,6 +29,23 @@ func _handle_user_playfield_click(mousePosition2D):
 	
 	var worldspace = get_world_3d().direct_space_state
 	var result = worldspace.intersect_ray(params)	
+	
+	if !result:
+		return
+	
+	if currentlySelectedPlayer != null:
+		currentlySelectedPlayer.set_target_position(result.position)
+			
+func _handle_user_agent_selection(mousePosition2D):
+	# If intersecting with PlayerAgent, select PlayerAgent and DON'T move. 
+	# Else, move currently selected PlayerAgent.
+	var params = PhysicsRayQueryParameters3D.new()
+	
+	params.from = currentCamera.project_ray_origin(mousePosition2D)
+	params.to = currentCamera.project_position(mousePosition2D, 1000)
+	
+	var worldspace = get_world_3d().direct_space_state
+	var result = worldspace.intersect_ray(params)	
 	var resultingCollider = result.collider
 	
 	if !resultingCollider:
@@ -36,9 +53,6 @@ func _handle_user_playfield_click(mousePosition2D):
 	
 	if resultingCollider is PlayerAgent:
 		_set_currently_selected_player(resultingCollider)
-	else:
-		if currentlySelectedPlayer != null:
-			currentlySelectedPlayer.set_target_position(result.position)
 
 func _set_currently_selected_player(selectedPlayer : PlayerAgent):
 	currentlySelectedPlayer = selectedPlayer
