5d26807800cd3158b9c31af2fb428bdc2e977db4
Brooklyn <brooklyn.dipietrantonio@gmail.com>
2026-04-20 13:50:04 -0500

    Main menu prog

---
+++ code/main_menu_controller.gd
@@ -0,0 +1,48 @@
+extends Node
+
+# Buttons
+@export var play_button : Button
+@export var settings_button : Button
+@export var credits_button : Button
+@export var exit_button : Button
+
+@export var level_select_canvas : CanvasItem
+@export var settings_canvas : CanvasItem
+@export var credits_canvas : CanvasItem
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play_button.pressed.connect(play)
+	settings_button.pressed.connect(settings)
+	credits_button.pressed.connect(credits)
+	exit_button.pressed.connect(exit)
+	
+	level_select_canvas.visible = false;
+	settings_canvas.visible = false;
+	credits_canvas.visible = false;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func play():
+	level_select_canvas.visible = true;
+	settings_canvas.visible = false;
+	credits_canvas.visible = false;
+	pass
+	
+func settings():
+	level_select_canvas.visible = false;
+	settings_canvas.visible = true;
+	credits_canvas.visible = false;
+	pass
+	
+func credits():
+	level_select_canvas.visible = false;
+	settings_canvas.visible = false;
+	credits_canvas.visible = true;
+	pass
+
+func exit():
+	pass
