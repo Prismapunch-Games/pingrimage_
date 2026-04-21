588c1ec0cba4caaeb2a30639e428123ca64ee4a3
Byemoh <baiomurang@gmail.com>
2026-04-20 19:00:26 -0500

    Update music_handler.gd

--- code/music_handler.gd
+++ code/music_handler.gd
@@ -3,9 +3,14 @@ extends AudioStreamPlayer3D
 
 func _ready():
 	Global.on_level_start.connect(_on_level_start)
+	Global.on_game_complete.connect(_on_game_complete)
 	
 func _on_level_start():
 	if(stream == Global.level_difficulties[Global.current_level_node.difficulty]):
 		return
 	stream = Global.level_difficulties[Global.current_level_node.difficulty]
+	play()
+
+func _on_game_complete():
+	stream = load("res://sounds/music/LudumDareBeatGame.mp3")
 	play()
